3c4d15923984168b89bd81a9f77804599db6855c
alex14072001 <aleko.gorjomeladze.01@gmail.com>
2024-07-26 17:05:36 +0400

    commit

---
+++ leasson1.py
@@ -0,0 +1,7 @@
+name= "ალეკო"
+surname= "ღორჯომელაძე"
+adress= "თბილისი ილია აბულაძის #15"
+age= "23 წლის"
+height= "სიმაღლე 180 სმ"
+
+print(name + " "+ surname + " " + adress + " " + age + " " + height)
