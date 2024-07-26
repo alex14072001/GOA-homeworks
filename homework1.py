f72fae4aa42e3b1288b75188fa64c52f8486f354
alex14072001 <aleko.gorjomeladze.01@gmail.com>
2024-07-26 15:55:14 +0400

    commit

---
+++ homework1.py
@@ -0,0 +1,52 @@
+from turtle import*
+
+speed(10)
+width(30)
+
+penup()
+goto(-160, 100)
+pendown()
+
+left(-45)
+forward(270)
+
+
+penup()
+goto(30, 100)
+pendown()
+
+left(-90)
+forward(270)
+
+penup()
+goto(-160, 100)
+pendown()
+
+left(180)
+forward(120)
+
+penup()
+goto(30, 100)
+pendown()
+
+left(-90)
+forward(120)
+
+
+penup()
+goto(30, -95)
+pendown()
+
+right(90)
+forward(120)
+
+
+penup()
+goto(-160, -90)
+pendown()
+
+right(90)
+forward(120)
+
+
+exitonclick()
