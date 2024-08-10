adde845c56b63386c07da162ba180ff0c089ed88
alex14072001 <aleko.gorjomeladze.01@gmail.com>
2024-08-10 18:18:43 +0400

    commit

---
+++ day1/lesson4.py/lesson4.py
@@ -0,0 +1,9 @@
+#Create a variable with two words separated by an underscore
+my_variable = "Hello, world!"
+# Print the value stored in the variable
+print(my_variable)
+
+#ახსნა:
+# 1. my_variable არის ცვლადის სახელი.
+# 2. "Hello, world!" არის სტრიქონის მნიშვნელობა, რომელიც მინიჭებულია ცვლადზე my_variable
+# 3. print(my_variable) გამოსცემს ცვლადის მნიშვნელობას კონსოლში.
