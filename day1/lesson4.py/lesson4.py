4b4b22646497b03b23c73f60c027e34f65ddb261
alex14072001 <aleko.gorjomeladze.01@gmail.com>
2024-08-10 18:31:16 +0400

    commit

--- day1/lesson4.py/lesson4.py
+++ day1/lesson4.py/lesson4.py
@@ -7,3 +7,30 @@ print(my_variable)
 # 1. my_variable არის ცვლადის სახელი.
 # 2. "Hello, world!" არის სტრიქონის მნიშვნელობა, რომელიც მინიჭებულია ცვლადზე my_variable
 # 3. print(my_variable) გამოსცემს ცვლადის მნიშვნელობას კონსოლში.
+
+
+
+#addition
+print(5 + 3)
+
+#subtraction
+print(5-3)
+
+#multiplication
+print(5*3)
+
+#division
+print(5/3)
+
+# Exponentiation
+print(5 ** 3) 
+
+
+# Create two variables to store numbers
+number1 = 8
+number2 = 4
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
